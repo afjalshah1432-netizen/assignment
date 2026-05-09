c13c7a7a93b84517e6ee87ae9ad5fbfe9675eda4
afjalshah1432-netizen <afjalshah1432@gmail.com>
2026-05-09 15:31:30 +0530

    added cnnDb

--- backend_old/server.js
+++ backend_old/server.js
@@ -3,10 +3,20 @@ import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import studentRoutes from "./routes/studentRoutes.js"
-import cnnDb from "./con/db.js"
 dotenv.config();
 
 const app=express();
+
+const cnnDb=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGOURL);
+        console.log("mongodb is connected");
+    }
+    catch(err){
+        console.log("mongodb is not conneted",err)
+    }
+}
+
 cnnDb();
 app.use(cors());
 app.use(helmet({
